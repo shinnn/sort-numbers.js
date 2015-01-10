54b7c49a10f5285c172c93c6a573c0122ea7d370
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-11 04:48:04 +0900

    improve build script

--- gulpfile.js
+++ gulpfile.js
@@ -12,54 +12,48 @@ var toCamelCase = require('to-camel-case');
 
 var pkg = require('./package.json');
 var bower = require('./bower.json');
-
 var funName = toCamelCase(pkg.name);
-
-var banner = [
-  '/*!',
-  ' * sort-numbers | MIT (c) Shinnosuke Watanabe',
-  ' * https://github.com/shinnn/sort-numbers.js',
-  '*/\n'
-].join('\n');
+var banner = require('tiny-npm-license')(pkg);
 
 gulp.task('lint', function() {
-  gulp.src(['{,src/}*.js'])
-    .pipe($.jshint())
-    .pipe($.jshint.reporter(stylish))
-    .pipe($.jscs('.jscs.json'));
-  gulp.src('*.json')
-    .pipe($.jsonlint())
-    .pipe($.jsonlint.reporter());
+  return mergeStream(
+    gulp.src(['{,src/}*.js'])
+      .pipe($.jshint())
+      .pipe($.jshint.reporter(stylish))
+      .pipe($.jscs(pkg.jscsConfig)),
+    gulp.src('*.json')
+      .pipe($.jsonlint())
+      .pipe($.jsonlint.reporter())
+  );
 });
 
 gulp.task('clean', rimraf.bind(null, 'dist'));
 
 gulp.task('build', ['lint', 'clean'], function() {
   return mergeStream(
-    gulp.src(['src/*.js'])
+    gulp.src('src/*.js')
       .pipe($.header(banner + '!function() {\n'))
       .pipe($.footer('\nwindow.' + funName + ' = ' + funName + ';\n}();\n'))
-      .pipe($.rename(bower.main))
-      .pipe(gulp.dest('')),
-    gulp.src(['src/*.js'])
+      .pipe($.rename(bower.main)),
+    gulp.src('src/*.js')
       .pipe($.header(banner))
       .pipe($.footer('module.exports = ' + funName + ';\n'))
       .pipe($.rename(pkg.main))
-      .pipe(gulp.dest(''))
-  );
+  )
+    .pipe(gulp.dest(''));
 });
 
 gulp.task('test', ['build'], function(cb) {
   exec('node test.js', function(err, stdout, stderr) {
-    console.warn(stderr);
-    console.log(stdout);
+    process.stdout.write(stdout);
+    process.stderr.write(stderr);
     cb(err);
   });
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['{,src/}*.js'], ['test']);
-  gulp.watch(['*.json', '.jshintrc'], ['lint']);
+  gulp.watch('{,src/}*.js', ['test']);
+  gulp.watch('*.json,.jshintrc', ['lint']);
 });
 
 gulp.task('default', ['test', 'watch']);
